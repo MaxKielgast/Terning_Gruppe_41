d6cb519ceaf01c3d00f82725eb37a7ad30925638
GrobaxGrabbleBlast <Hansbyager@gmail.com>
2018-10-05 12:15:36 +0200

    JEG er træt at git!.....

--- src/terning/Dice.java
+++ src/terning/Dice.java
@@ -3,17 +3,14 @@ package terning;
 public class Dice {
 	// roll the die and return the value (1-6)
 	public int roll() {
-		float d1=(float)Math.random();     // 0-1
-		float d2=d1*5;                     // 0-5
-		int d3=Math.round(d2);             // 0-5 integer
+		float d1 = (float)Math.random();     // 0-1
+		float d2 = d1*5;                     // 0-5
+		int d3 = Math.round(d2);             // 0-5 integer
 		return d3 + 1;                     // 1-6		
 	}
-	// ikke halløj!! igen hej
 
-	// roll the die n times and print the values
 	public void rollMultiple(int n) {
-		int MarkWasHere = 69;
-		for (int i=1; i<n; i++) {
+		for (int i = 1 ; i < n ; i++) {
 			System.out.print(roll() + " ");
 		}		
 	}
--- src/terning/DiceTest.java
+++ src/terning/DiceTest.java
@@ -4,11 +4,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DiceTest {
 
+
     @org.junit.jupiter.api.Test
     void roll() {
+        Dice TestDice = new Dice();
+        // gentagelse et par gang
+        for (int i = 0; i < 100 ; i++) {
 
+            int x = TestDice.roll();
+            boolean y = false;
+
+            if(x <= 6 && x >= 1){
+                y = true;
+            }
+
+            // hvis y er sand, så er der ingen fejl.
+            assertTrue(y);
+        }
     }
-
     @org.junit.jupiter.api.Test
     void rollMultiple() {
         
